81fc4cf995a67a55a3c8d5bb105bda66a21ce7c5
PIERRON Marc <pierronmarc@outlook.fr>
2022-08-31 19:27:06 +0200

    build: install and configure fonts 'Roboto', 'La Belle Aurore' and 'Montserrat'

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'montserrat': ['Montserrat'],
+        'la-belle-aurore': ['La Belle Aurore'],
+        'roboto': ['Roboto']
+      }
+    },
   },
   plugins: [],
 }
