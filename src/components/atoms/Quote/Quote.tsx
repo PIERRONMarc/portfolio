0fd38173f57dfdf72ab2acbd547286f031e31241
PIERRON Marc <pierronmarc@outlook.fr>
2022-09-13 00:21:09 +0200

    refactor: change quote

--- src/components/atoms/Quote/Quote.tsx
+++ src/components/atoms/Quote/Quote.tsx
@@ -1,7 +1,7 @@
 function Quote() {
     return (
         <>
-            <div className="font-montserrat text-xl sm:text-right sm:text-2xl italic">“Truth can only be found in one place: <span className="text-accent">the code</span>”</div>
+            <div className="font-montserrat text-xl sm:text-right sm:text-2xl italic">"It is not enough for code <span className="text-accent">to work.</span>"</div>
             <div className="font-montserrat text-xl sm:text-right sm:text-2xl">- Robert C. Martin</div>
         </>
     )
