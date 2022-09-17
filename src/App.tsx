4dfe536d397f9af0800047561bba7aedd2a75b94
PIERRON Marc <pierronmarc@outlook.fr>
2022-09-17 19:34:25 +0200

    feat: add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,14 @@ import HomePage from "./components/pages/Home/HomePage";
 import {HashRouter, Route, Routes} from "react-router-dom";
 import ProjectPage from "./components/pages/Project/ProjectPage";
 import {Project, projects} from "./utils/Projects";
+import { init, track, Types } from '@amplitude/analytics-browser';
+
+init('e0d700db356f1c500480cf5e321476be', undefined, {
+    disableCookies: true, // Disables the use of browser cookies
+    serverZone: Types.ServerZone.EU, // Send data to Amplitude's EU servers
+});
+
+track('visit');
 
 function App() {
   return (
