38aab530814d24ce8643569c1efc5029c72fbb75
PIERRON Marc <pierronmarc@outlook.fr>
2022-09-18 17:27:04 +0200

    feat: add reveal animation on project page

--- src/components/pages/Project/ProjectPage.tsx
+++ src/components/pages/Project/ProjectPage.tsx
@@ -23,55 +23,53 @@ function ProjectPage(props: ProjectPageProps) {
     }, [])
 
     return (
-        <>
-            <div className="container mx-auto px-4 sm:px-0">
-                <Header showHomeLink={true} />
-                <main>
-                    <div className="text-primary flex flex-col md:flex-row md:items-start mt-10 mb-2">
-                        <div className="font-roboto font-thin text-sm md:order-2 md:pt-1 md:ml-2 pl-1 md:pl-0">
-                            {year}. <span className="text-accent">{category}</span>
-                        </div>
-
-                        <div className="text-5xl md:text-7xl cursor-pointer">
-                            {title}
-                        </div>
+        <div className="container mx-auto px-4 sm:px-0 animate-reveal">
+            <Header showHomeLink={true} />
+            <main>
+                <div className="text-primary flex flex-col md:flex-row md:items-start mt-10 mb-2">
+                    <div className="font-roboto font-thin text-sm md:order-2 md:pt-1 md:ml-2 pl-1 md:pl-0">
+                        {year}. <span className="text-accent">{category}</span>
                     </div>
 
-                    {tags && (
-                        <TagList tags={tags} className="mb-10" />
-                    )}
+                    <div className="text-5xl md:text-7xl cursor-pointer">
+                        {title}
+                    </div>
+                </div>
 
-                    <p className="text-primary mb-10">{description}</p>
+                {tags && (
+                    <TagList tags={tags} className="mb-10" />
+                )}
 
-                    {codeLink && (
-                        <p className={"text-primary font-montserrat mb-5"}>
-                            <a href={codeLink} rel="noreferrer" target="_blank" className=" underline hover:no-underline hover:text-accent ease-in duration-150">See code</a>
-                        </p>
-                    )}
+                <p className="text-primary mb-10">{description}</p>
 
-                    {seeMoreLink && (
-                        <p className={"text-primary font-montserrat mb-5"}>
-                            <a href={seeMoreLink} rel="noreferrer" target="_blank" className=" underline hover:text-accent hover:no-underline ease-in duration-150">See more</a>
-                        </p>
-                    )}
+                {codeLink && (
+                    <p className={"text-primary font-montserrat mb-5"}>
+                        <a href={codeLink} rel="noreferrer" target="_blank" className=" underline hover:no-underline hover:text-accent ease-in duration-150">See code</a>
+                    </p>
+                )}
 
-                    <Swiper
-                        keyboard={true}
-                        navigation={true}
-                        pagination={{
-                            bulletActiveClass: "!bg-accent swiper-pagination-bullet-active"
-                        }}
-                        modules={[Navigation, Pagination, Keyboard]}
-                        className="mb-10"
-                    >
-                        {screens.map((screen: string, key: number) => (
-                            <SwiperSlide key={key}><img src={screen} alt="project screen" className="mx-auto" style={{maxWidth: '100%', maxHeight: '80vh'}}/></SwiperSlide>
-                        ))}
-                    </Swiper>
-                </main>
-                <Footer className="my-5" />
-            </div>
-        </>
+                {seeMoreLink && (
+                    <p className={"text-primary font-montserrat mb-5"}>
+                        <a href={seeMoreLink} rel="noreferrer" target="_blank" className=" underline hover:text-accent hover:no-underline ease-in duration-150">See more</a>
+                    </p>
+                )}
+
+                <Swiper
+                    keyboard={true}
+                    navigation={true}
+                    pagination={{
+                        bulletActiveClass: "!bg-accent swiper-pagination-bullet-active"
+                    }}
+                    modules={[Navigation, Pagination, Keyboard]}
+                    className="mb-10"
+                >
+                    {screens.map((screen: string, key: number) => (
+                        <SwiperSlide key={key}><img src={screen} alt="project screen" className="mx-auto" style={{maxWidth: '100%', maxHeight: '80vh'}}/></SwiperSlide>
+                    ))}
+                </Swiper>
+            </main>
+            <Footer className="my-5" />
+        </div>
     )
 }
 
