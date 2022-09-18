17641d4a5d4eaf5be727a0637d9448696aed1f41
PIERRON Marc <pierronmarc@outlook.fr>
2022-09-18 17:13:14 +0200

    refactor: remove useless code and add className prop

--- src/components/molecules/Header/Header.tsx
+++ src/components/molecules/Header/Header.tsx
@@ -2,21 +2,16 @@ import HtmlTag from "../../atoms/HtmlTag/HtmlTag";
 import {Link} from "react-router-dom";
 
 interface HeaderProps {
-    showHomeLink?: boolean
+    showHomeLink?: boolean,
+    className?: string
 }
 
 function Header(props: HeaderProps) {
 
-    const { showHomeLink } = props;
+    const { showHomeLink, className } = props;
 
     return (
-        // <header className="pt-5">
-        //     <HtmlTag className="block">&lt;html&gt;</HtmlTag>
-        //     <HtmlTag className="ml-5">&lt;body&gt;</HtmlTag>
-        //
-        // </header>
-
-        <header className="flex flex-row justify-between">
+        <header className={`flex flex-row justify-between ${className}`}>
             <div className="pt-5">
                 <HtmlTag className="block">&lt;html&gt;</HtmlTag>
                 <HtmlTag className="ml-5">&lt;body&gt;</HtmlTag>
